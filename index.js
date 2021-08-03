6b20cc4487b8c2595619ad999bc9b2510ed5c07a
janchri <jans.christoph@gmail.com>
2021-08-03 09:21:18 +0200

    axios added

--- index.js
+++ index.js
@@ -1,79 +1,109 @@
+const uri_wirbelwind_box = "http://192.168.188.44"
+const headers = { "Content-Type" : "application/json" }
+
 const { createApp, reactive } = Vue
 
 const networks = createApp({
 	data() {
 		return {
-			wifis : []
+			wifis : [],
+			password: '',
+			bluetooth_state : false
+		}
+	},
+	mounted(){
+		//axios.get(uri_wirbelwind_box+"/networks?list=active", {headers} )
+		//.then(response => this.wifis = response.data.list_active_wifis)
+		//.catch(error => {
+		//	this.errorMessage = error.message;
+		//	console.error("There was an error!", error);
+		//})
+	},
+	watch: {
+		bluetooth_state(state){
+			console.log(state);
 		}
 	},
 	methods: {
-		bluetooth() {
-			console.log("Bluetooth Toogle")
-		},
-		scanActiveNetworks() {
-			console.log("Scanning WiFis")
-		}
+		addNetwork: async function (event, ssid) {
+			const response = await axios.post(uri_wirbelwind_box+"/networks",
+				{ SSID : ssid,
+					PWD : event.target.value
+				});
+  	}
 	}
 }).mount('#app-networks')
 
 const current_playlist = reactive({
+	uuid : '',
+	name : '',
 	tracks : [],
 	addTrack(path) {
-		console.log("addTrack(): "+path)
+		axios.post(uri_wirbelwind_box+"/playlist",
+			{
+				uuid : this.uuid,
+				add : [path]
+			})
+			.then(response => this.tracks = response.data[0].tracks)
+	    .catch(error => {
+	      this.errorMessage = error.message;
+	      console.error("There was an error!", error);
+	    });
 	},
-	deleteTrack(idx) {
-		console.log("deleteTrack(): "+idx)
+	async deleteTrack(path) {
+		const response = await axios.post(uri_wirbelwind_box+"/playlist",
+			{ uuid : this.uuid,
+				delete : [path]
+			});
+			this.tracks = response.data[0].tracks;
 	},
-	setName(name){
-		console.log("setName(): "+name)
+	async setName(){
+		console.log("setName(): ");
+		console.log("uuid: "+this.uuid);
+		console.log("name: "+this.name);
+		const response = await axios.post(uri_wirbelwind_box+"/playlist",
+			{ uuid : this.uuid,
+				name : this.name
+			});
+		this.name = response.data[0].name;
 	},
 	reset(){
+		console.log("uuid: "+this.uuid)
+		console.log("name: "+this.name)
 		console.log("reset(): "+name)
 	}
 })
 
 const playlists = createApp({
-	template: `
-	<div id="id_playlists">
-	<select v-model="selected_playlist">
-	<option disabled value="">Choose playlist</option>
-	<option v-for="playlist in playlists" :value="playlist.uuid">
-	{{ playlist.name }}
-	</option>
-	</select>
-	</div>
-	<p>Tracks</p>
-	<div id="id_tracks" v-for="(track,index) in current_playlist.tracks">
-	<button @click="current_playlist.deleteTrack(index)">{{ track }}</button>
-	</div>
-	`
-	,
 	data() {
 		return {
 			playlists: [],
-			selected_playlist : '',
+			current_playlist_uuid : '',
 			current_playlist
 		}
 	},
 	created() {
-		fetch("http://wirbelwind.box/playlist")
-		.then(response => response.json())
-		.then(data => (this.playlists = data));
+		axios.get(uri_wirbelwind_box+"/playlist", {headers} )
+			.then(response => this.playlists = response.data)
+			.catch(error => {
+				this.errorMessage = error.message;
+				console.error("There was an error!", error);
+
+			})
 	},
 	watch: {
-		selected_playlist(val){
-			console.log(val)
-			const requestOptions = {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify({ uuid: val })
-			};
-			fetch("http://wirbelwind.box/playlist", requestOptions)
-			.then(response => response.json())
-			.then(data => (this.current_playlist.tracks = data[0].tracks));
+		async current_playlist_uuid(uuid){
+			const playlist = { uuid : uuid};
+			const response = await axios.post(uri_wirbelwind_box+"/playlist", playlist);
+    	this.current_playlist.uuid = response.data[0].uuid;
+    	this.current_playlist.name = response.data[0].name;
+    	this.current_playlist.tracks = response.data[0].tracks;
 		}
+	},
+	methods:{
+		onSubmitSetName() {
+			current_playlist.setName();
+	  }
 	}
 })
 playlists.mount('#app-playlists')
@@ -86,7 +116,7 @@ const files = createApp({
 		}
 	},
 	created() {
-		fetch('http://wirbelwind.box/files?path=/')
+		fetch(uri_wirbelwind_box+"/files?path=/")
 		.then(response => response.json())
 		.then(data => (this.treeData = data));
 	}
@@ -134,7 +164,7 @@ files.component("tree-item", {
 			}
 		},
 		loadChildren() {
-			fetch("http://wirbelwind.box/files?path="+this.item.path)
+			fetch(uri_wirbelwind_box+"/files?path="+this.item.path)
 			.then(response => response.json())
 			.then(data => this.item.children = data);
 		},
